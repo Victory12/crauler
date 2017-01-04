1060fe335d3cea9ec544b4441ef53c8aa132948e
Victory 12 <suslova_vichka@mail.ru>
2017-01-04 22:46:13 +0300

    finall

--- spider2.pl
+++ spider2.pl
@@ -4,11 +4,11 @@ use warnings;
 use DDP;
 use AnyEvent::HTTP;
 use AnyEvent;
-use v5.10;
 use HTML::LinkExtractor;
+use v5.10;
 my @urls = qw(
-	http://www.mosaquarium.ru);
-my $count = 0;
+	http://www.coffeehouse.ru);
+my $count = 1;
 my $cv = AnyEvent->condvar;
 my @size_urls;
 
@@ -16,29 +16,33 @@ my $max = 4;
 my $guard;
 $guard = http_get $urls[0], sub {
 	my ($html) = @_; 
-	say "$urls[0] $count received, Size: ", length $html;
 	push @size_urls, {url => $urls[0], size => length $html};
 	@urls = (@urls, parser($html, @urls)) if $#urls < 10000;
-	$count+=1;
 	for (1 .. $max) {
 	   send_url();
 	}	
 };
 
 $cv->recv;
+print_info (@size_urls);
 
-@size_urls = reverse sort { $a->{size} <=> $b->{size} } @size_urls;
-my $num = 0;
-my $all_size = 0;
-say "top of pages by size";
-while ($num < 10) {
-	p $size_urls[$num];
-	$num++;
-}	
-for my $page (@size_urls){
-	$all_size += $page->{size};
+
+sub print_info{
+	my @size_urls = @_;
+	my $num = 0;
+	my $all_size = 0;
+	@size_urls = reverse sort { $a->{size} <=> $b->{size} } @size_urls;
+	say "top of pages by size";
+	while ($num < 10) {
+		p $size_urls[$num];
+		$num++;
+	}	
+	for my $page (@size_urls){
+		$all_size += $page->{size};
+	}
+	say "ALL SIZE is $all_size";
 }
-say "ALL SIZE is $all_size";
+
 sub send_url {
 	return if $#size_urls >= 10000;
 	my $url = $urls[$count];
@@ -48,7 +52,7 @@ sub send_url {
 	$cv->begin;
 	http_get $url, sub {
 		my ($html) = @_;
-		say "$url received, Size: ", length $html;
+		say "In work $url";
 		push @size_urls, {url => $url, size => length $html};
 		@urls = (@urls, parser($html, @urls)) if $#urls < 10000;
 		$cv->end;
@@ -58,8 +62,7 @@ sub send_url {
 }
 
 sub parser {
-	my $html = shift;
-   
+	my $html = shift; 
     my @urls = @_;
     my $url = $urls[0];
 	my $LX = new HTML::LinkExtractor();
@@ -70,12 +73,12 @@ sub parser {
     my @result;
     for my $link (@array){
     	if ( defined $link->{href} and $link->{tag} eq 'a' ){    		
-    		if ($link->{href} =~ m[^$url.+]){  
+    		if ($link->{href} =~ m[^$url.+] and $link->{href} =~ m[^(?!.*(mp3|gif|jpg|pdf|doc)).*$]){  
 	    		unless ( $link->{href} ~~ @urls )   {
 	       	 		push @result, $link->{href};
 	       		}    				
     		}
-    		elsif ($link->{href} =~ m[^/.+] and $link->{href} =~ m[^(?!.*(www|http)).*$]){			
+    		elsif ($link->{href} =~ m[^/.+] and $link->{href} =~ m[^(?!.*(www|http|jpg|pdf|doc)).*$]){			
     			$link->{href} =~ s[^/(.*)][$url/$1];
     			unless ( $link->{href} ~~ @urls )   {
 	       	 		push @result, $link->{href};
@@ -86,5 +89,4 @@ sub parser {
     my %hash;
     @result = grep{!$hash{$_}++} @result;
    	return @result;
-
 } 
