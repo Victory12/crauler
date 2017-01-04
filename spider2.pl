ee28827ee11f9a177b671865c9e866c303bb49b0
Victory 12 <suslova_vichka@mail.ru>
2017-01-04 20:18:28 +0300

    four

--- spider2.pl
+++ spider2.pl
@@ -5,13 +5,9 @@ use DDP;
 use AnyEvent::HTTP;
 use AnyEvent;
 use v5.10;
-#change to parse first page
-#	http://pandok.ru/zakazat-banket
-#	http://pandok.ru/sezonnoe-menyu
-#	http://pandok.ru/sezonnoe-menyu/product/221-salat-letnij
-#);
+use HTML::LinkExtractor;
 my @urls = qw(
-	http://pandok.ru/);
+	http://tkachov-musey.ru);
 my $count = 0;
 my $cv = AnyEvent->condvar;
 my @size_urls;
@@ -19,25 +15,19 @@ my @size_urls;
 my $max = 4;
 my $guard;
 $guard = http_get $urls[0], sub {
-		my ($html) = @_;
-		 
-		p @urls;
-		say "$urls[0] $count received, Size: ", length $html;
-		push @size_urls, {url => $urls[0], size => length $html};
-		@urls = (@urls, get_data($html, $urls[0], @urls)) if $#urls < 10000;
-		p @urls;
-		#undef $guard;
-		$count+=1;
-		for (1 .. $max) {
-		   send_url();
-		}
-		
-	};
-
-
+	my ($html) = @_; 
+	say "$urls[0] $count received, Size: ", length $html;
+	push @size_urls, {url => $urls[0], size => length $html};
+	@urls = (@urls, parser($html, @urls)) if $#urls < 1000;
+	#p @urls;
+	$count+=1;
+	for (1 .. $max) {
+	   send_url();
+	}	
+};
 
 $cv->recv;
- 
+p @size_urls;
  
 sub send_url {
 	return if $#size_urls >= 10000;
@@ -50,27 +40,45 @@ sub send_url {
 		my ($html) = @_;
 		say "$url received, Size: ", length $html;
 		push @size_urls, {url => $url, size => length $html};
-		@urls = (@urls, get_data($html, $url, @urls)) if $#urls < 10000;
+		@urls = (@urls, parser($html, @urls)) if $#urls < 1000;
 		$cv->end;
 		send_url();
 	};
 	return 1;
 }
-# normal parser
-sub get_data{
-    my $body = shift;
-    my $url = shift;
+
+sub parser {
+	my $html = shift;
+   
     my @urls = @_;
-    my @array;
-    $body =~ s[<head>(.*)</head>][]s;
-    while($body =~ s[href="($url[^"]*|/[^"]*)"][]s){
-       my $str = $1;
-       $str =~ s[^/(.*)][http://pandok.ru/$1];      
-       unless ( $str ~~ @urls )   {
-       	  push @array,$str unless $str =~ /search/;
-       }  
+    my $url = $urls[0];
+	my $LX = new HTML::LinkExtractor();
+    $LX->parse(\$html);
+    my $links = $LX->links;
+    return undef unless defined $links->[0];
+    my @array = @$links;
+    my @result;
+    for my $link (@array){
+    	#p $link;
+    	if ( defined $link->{href} and $link->{tag} eq 'a' ){    		
+    		if ($link->{href} =~ m[^$url.+]){  
+	    		unless ( $link->{href} ~~ @urls )   {
+	       	 		push @result, $link->{href};
+	       		}    				
+    		}
+    		if ($link->{href} =~ m[^/[^h].+]){
+				#say "HERE $link->{href} $link->{_TEXT}";
+    			$link->{href} =~ s[^/(.*)][$url/$1];
+    			#say "HERE $1";
+    			unless ( $link->{href} ~~ @urls )   {
+	       	 		push @result, $link->{href};
+	       		}  
+    		}
+    	}
     }
     my %hash;
-    @array=grep{!$hash{$_}++} @array;
-    return @array;
-}
+    @result = grep{!$hash{$_}++} @result;
+   	return @result;
+
+} 
+#if
